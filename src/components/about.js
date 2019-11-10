5f69028ba3d73822633309148cf31692ef89e956
ktw1016 <ktw_1016@hotmail.com>
2019-11-10 12:56:07 -0500

    minor warning fixes

--- src/components/about.js
+++ src/components/about.js
@@ -10,17 +10,17 @@ export default class About extends React.Component {
         return (
             <div className='about_container'>
                 <div className="media_container">
-                    <a className="col_border" href="mailto:kang.taewan.96@gmail.com" target="_blank">
+                    <a className="col_border" href="mailto:kang.taewan.96@gmail.com" target="_blank" rel="noopener noreferrer">
                         <img src={email} alt="email" height="25" width="25"/>
                     </a>
-                    <a className="col_border" href="https://github.com/ktw1016" target="_blank">
+                    <a className="col_border" href="https://github.com/ktw1016" target="_blank" rel="noopener noreferrer">
                         <img src={github} alt="github" height="25" width="25"/>
                     </a>
-                    <a className="col_border" href="https://www.linkedin.com/in/taewan-kang-605060195/" target="_blank">
+                    <a className="col_border" href="https://www.linkedin.com/in/taewan-kang-605060195/" target="_blank" rel="noopener noreferrer">
                         <img src={linkedin} alt="linkedin" height="25" width="25"/>
                     </a>
                 </div>
-                <span className="text" style={{"font-size": 70}}> TAEWAN KANG </span>
+                <span className="text" style={{"fontSize": 70}}> TAEWAN KANG </span>
                 <span className="text"> SOFTWARE ENGINEER </span>
                 <span className="text"> BORN IN SEOUL, LIVING IN OTTAWA </span>
             </div>
