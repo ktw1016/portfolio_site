ab790b5d2e8bccf462909c3a11c35ed21de233e3
ktw1016 <ktw_1016@hotmail.com>
2019-11-10 21:00:48 -0500

    minor changes, another version of myname svg

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,7 @@ import github from '../svg/github-logo.svg';
 import linkedin from '../svg/linkedin.svg';
 import email from '../svg/email.svg';
 import myname from '../svg/myname.svg';
+//import myname from '../svg/myname_ver2.svg';
 
 import React from 'react';
 import anime from "animejs";
@@ -17,7 +18,7 @@ export default class About extends React.Component {
     const textWrapper2 = document.getElementById('about_desc_text2');
     wrap_letters(textWrapper1);
     wrap_letters(textWrapper2);
-    
+
     const timeline = anime.timeline();
     timeline
       .add({
@@ -35,7 +36,7 @@ export default class About extends React.Component {
       })
       .add({
         targets: document.getElementById("line1"),
-        translateX: [0, textWrapper1.getBoundingClientRect().width - 20],
+        translateX: [0, textWrapper1.getBoundingClientRect().width + 5],
         easing: "easeOutExpo",
         duration: 1000,
       })
@@ -62,17 +63,17 @@ export default class About extends React.Component {
       })
       .add({
         targets: document.getElementById("line2"),
-        translateX: [0, textWrapper2.getBoundingClientRect().width - 40],
+        translateX: [0, textWrapper2.getBoundingClientRect().width + 5],
         easing: "easeOutExpo",
-        duration: 2300,
+        duration: 2100,
       })
       .add({
         targets: textWrapper2.children,
         opacity: [0,1],
-        duration: 800,
+        duration: 1800,
         easing: 'easeOutExpo',
         delay: (el, i) => 34 * (i+1),
-      }, '-=2275')
+      }, '-=2075')
       .add({
         targets: document.getElementById("line2"),
         scaleY: [1,0],
@@ -85,6 +86,15 @@ export default class About extends React.Component {
   render() {
     return (
       <div className='about_container'>
+        <img id="myname" src={myname} alt="myname" height="125" width="1025"/>
+        <div className="row_container">
+          <span id="line1" className="line"></span>
+          <span id="about_desc_text1" className="text"> SOFTWARE ENGINEER </span>
+        </div>
+        <div className="row_container">
+          <span id="line2" className="line"></span>
+          <span id="about_desc_text2" className="text"> BORN IN SEOUL, LIVING IN OTTAWA </span>
+        </div>
         <div id="media_row" className="row_container">
           <a className="col_border" href="mailto:kang.taewan.96@gmail.com" target="_blank" rel="noopener noreferrer">
             <img src={email} alt="email" height="25" width="25"/>
@@ -96,16 +106,8 @@ export default class About extends React.Component {
             <img src={linkedin} alt="linkedin" height="25" width="25"/>
           </a>
         </div>
-        <img id="myname" src={myname} alt="myname" height="125" width="1025"/>
-        <div className="row_container">
-          <span id="line1" className="line"></span>
-          <span id="about_desc_text1" className="text"> SOFTWARE ENGINEER </span>
-        </div>
-        <div className="row_container">
-          <span id="line2" className="line"></span>
-          <span id="about_desc_text2" className="text"> BORN IN SEOUL, LIVING IN OTTAWA </span>
-        </div>
       </div>
+      
     );
   }
 }
